92e37ecd6b91fcb7892658e61f693a96084dfe3f
gulcht <pn.glcht@gmail.com>
2025-11-16 20:39:20 +0700

    update line number count style

--- nvim/lua/plugins/gemini.lua
+++ nvim/lua/plugins/gemini.lua
@@ -1,16 +1,16 @@
 return {
-  {
-    "kiddos/gemini.nvim",
-    opts = {
-      hints = {
-        insert_result_key = "S-<Tab>",
-      },
-      completion = {
-        insert_result_key = "<S-Tab>",
-      },
-      instruction = {
-        menu_key = "<Leader>cg",
-      },
+    {
+        "kiddos/gemini.nvim",
+        opts = {
+            hints = {
+                insert_result_key = "S-<Tab>",
+            },
+            completion = {
+                insert_result_key = "<S-Tab>",
+            },
+            instruction = {
+                menu_key = "<Leader>cg",
+            },
+        },
     },
-  },
 }
