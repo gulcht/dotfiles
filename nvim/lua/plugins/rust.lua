6fa4ca9d88870794ca1e565c6d3298066244e375
Gu <141290593+gulcht@users.noreply.github.com>
2025-11-27 12:00:08 +0700

    remove rust-analyzer from mason

--- nvim/lua/plugins/rust.lua
+++ nvim/lua/plugins/rust.lua
@@ -1,4 +1,17 @@
 return {
+
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                bacon_ls = {
+                    enabled = diagnostics == "bacon-ls",
+                },
+                rust_analyzer = { enabled = false },
+            },
+        },
+    },
+
     {
         'mrcjkb/rustaceanvim',
         version = '^6',
